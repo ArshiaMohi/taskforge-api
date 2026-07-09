6b1d504e8885c13f5806b15914e67e53667d11c0
arshiamohi675@gmail.com <arshiamohi675@gmail.com>
2026-07-09 11:32:41 +0330

    chore: create initial package structure

--- src/main/java/com/arshia/taskforgeapi/TaskForgeApiApplication.java
+++ src/main/java/com/arshia/taskforgeapi/TaskForgeApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.taskforgeapi;
+package com.arshia.taskforgeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/arshia/taskforgeapi/TaskForgeApiApplicationTests.java
+++ src/test/java/com/arshia/taskforgeapi/TaskForgeApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.taskforgeapi;
+package com.arshia.taskforgeapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
