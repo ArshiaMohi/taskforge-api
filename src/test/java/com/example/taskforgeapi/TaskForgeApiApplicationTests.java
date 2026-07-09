f68d7c56025f381c9e589f4ef994942eb8721da4
arshiamohi675@gmail.com <arshiamohi675@gmail.com>
2026-07-09 11:09:12 +0330

    chore: initialize TaskForge project

---
+++ src/test/java/com/example/taskforgeapi/TaskForgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskforgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForgeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
